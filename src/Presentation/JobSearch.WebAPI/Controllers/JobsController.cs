e8e5efd138bc7fc98a0537107fd83c73b7fe833c
samiayazz <ayazzsami@gmail.com>
2023-12-12 19:39:41 +0300

    Test JobRepository

--- src/Presentation/JobSearch.WebAPI/Controllers/JobsController.cs
+++ src/Presentation/JobSearch.WebAPI/Controllers/JobsController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using JobSearch.Application.Contracts.Persistence.Repositories;
+using Microsoft.AspNetCore.Mvc;
 
 namespace JobSearch.WebAPI.Controllers;
 
@@ -6,9 +7,14 @@ namespace JobSearch.WebAPI.Controllers;
 [ApiController]
 public class JobsController : ControllerBase
 {
+    private IJobRepository _repository;
+
+    public JobsController(IJobRepository repository)
+        => _repository = repository;
+
     [HttpGet("/[action]", Name = "JobsGetAll")]
     public IActionResult GetAll()
     {
-        return Ok();
+        return Ok(_repository.GetAll());
     }
 }
