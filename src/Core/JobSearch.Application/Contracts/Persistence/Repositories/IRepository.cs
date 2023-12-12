8bd275a476b0408d3d5307488445d68235b84716
samiayazz <ayazzsami@gmail.com>
2023-12-12 19:12:52 +0300

    Add Repository Application

---
+++ src/Core/JobSearch.Application/Contracts/Persistence/Repositories/IRepository.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq.Expressions;
+using JobSearch.Application.Contracts.Domain;
+
+namespace JobSearch.Application.Contracts.Persistence.Repositories;
+
+public interface IRepository<TEntity, TKey>
+    where TEntity : class, IEntity, new()
+    where TKey : class, new()
+{
+    public ICollection<TEntity> GetAll(Expression<Func<TEntity, bool>> predicate = null);
+    public TEntity GetSingle(Expression<Func<TEntity, bool>> predicate);
+    public TEntity GetById(TKey id);
+    public void Create(TEntity entity);
+    public void CreateRange(ICollection<TEntity> entities);
+    public void Update(TEntity entity);
+    public void UpdateRange(ICollection<TEntity> entities);
+    public void Remove(TEntity entity);
+    public void RemoveRange(ICollection<TEntity> entities);
+    public void RemoveById(TKey id);
+}
---
+++ src/Infrastructure/JobSearch.Persistence/Repositories/Common/RepositoryBase.cs
@@ -0,0 +1,77 @@
+﻿using System.Linq.Expressions;
+using JobSearch.Application.Contracts.Domain;
+using JobSearch.Application.Contracts.Persistence.Repositories;
+using JobSearch.Persistence.Contexts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace JobSearch.Persistence.Repositories.Common;
+
+public abstract class RepositoryBase<TEntity, TKey> : IRepository<TEntity, TKey>
+    where TEntity : class, IEntity, new()
+    where TKey : class, new()
+{
+    private readonly JobSearchDbContext _dbContext;
+    private readonly DbSet<TEntity> _table;
+
+    public RepositoryBase(JobSearchDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public ICollection<TEntity> GetAll(Expression<Func<TEntity, bool>> predicate = null)
+    {
+        IQueryable<TEntity> query = _table;
+        if (predicate != null)
+            query.Where(predicate);
+
+        return query.ToList();
+    }
+
+    public TEntity GetSingle(Expression<Func<TEntity, bool>> predicate)
+        => _table.SingleOrDefault(predicate);
+
+    public TEntity GetById(TKey id)
+        => _table.Find(id);
+
+    public void Create(TEntity entity)
+    {
+        _table.Add(entity);
+        _dbContext.SaveChanges();
+    }
+
+    public void CreateRange(ICollection<TEntity> entities)
+    {
+        _table.AddRange(entities);
+        _dbContext.SaveChanges();
+    }
+
+    public void Update(TEntity entity)
+    {
+        _table.Update(entity);
+        _dbContext.SaveChanges();
+    }
+
+    public void UpdateRange(ICollection<TEntity> entities)
+    {
+        _table.UpdateRange(entities);
+        _dbContext.SaveChanges();
+    }
+
+    public void Remove(TEntity entity)
+    {
+        _table.Remove(entity);
+        _dbContext.SaveChanges();
+    }
+
+    public void RemoveRange(ICollection<TEntity> entities)
+    {
+        _table.RemoveRange(entities);
+        _dbContext.SaveChanges();
+    }
+
+    public void RemoveById(TKey id)
+    {
+        throw new NotImplementedException();
+    }
+}
